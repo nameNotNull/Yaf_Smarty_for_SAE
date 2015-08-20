b282fb56d37aa0890cfb67a74084f5485f734232
wangxuan <865387432@qq.com>
2015-08-21 00:17:26 +0800

    init

---
+++ application/controllers/Index.php
@@ -0,0 +1,40 @@
+<?php
+
+class IndexController extends Yaf_Controller_Abstract {
+    public function indexAction() { //默认Action
+        //test db library
+        $dbObj = new Db_Table(SAE_MYSQL_DB, 'user');
+        $res = $dbObj->getCount('id=3');
+        var_dump($res);
+        $field = array();
+        $whereArr = array(
+            'order' => 'id',
+            'limit' => '3'
+        );
+        $res = $dbObj->getAll($field, $whereArr);
+        var_dump($res);
+        //test smarty library
+        $this->getView()->assign("contentaa", "Hello Worldssss");
+        //test zendDb library
+        $db = Yaf_Registry::get('db');
+        $sql = $db->quoteInto('select * from usertest where user_id <?', '3');
+        $result = $db->query($sql);
+        $res = $result->fetchAll();
+
+        
+        var_dump($res);
+        /*
+        $adapter = new Zend_Db_Table();
+        $adapter->setDefaultAdapter($db);
+        $obj = new UserTableModel();
+        $data = array(
+            'id' => '1',
+            'name' => 'King',
+            'author' => 'Arthur',
+            'color' => 'blue'
+        );
+        $obj->insert($data);*/
+    }
+}
+?>
+
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+define("APP_PATH",  realpath(dirname(__FILE__) )); /* 指向public的上一级 */
+define("APPLICATION_PATH",  realpath(APP_PATH.'/application'));
+define("SMARTY_PATH",  realpath(APP_PATH.'/application/library'));
+define("SMARTY_SYSPLUGINS_DIR",  realpath(APP_PATH.'/application/library/sysplugins'));
+define("LIBRARY_PATH",  realpath(APP_PATH.'/application/library'));
+$app  = new Yaf_Application(APP_PATH . "/conf/application.ini",'product');
+$app->bootstrap()->run();
+
